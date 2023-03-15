21b91ab0c19fbe9ea818420342dfd97f19885347
Stefanos T <sttqueries@gmail.com>
2023-03-15 23:17:51 +0000

    Silly Experiment using Parallel for each instead
    The processing speed is huge but not fully tested for any compatibility.

--- OsuOszExtractor/OsuOszExtractor.cs
+++ OsuOszExtractor/OsuOszExtractor.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace OsuOszExtractor
 {
     public class OsuOszExtrator
     {
         private const string OSU_FILE_EXTENSION = ".osu";
-        public static void ExtractOsz(string path,string outputDir)
+        public static void ExtractOsz(string path, string outputDir)
         {
             if (IsOsuFile(path))
             {
@@ -19,23 +16,24 @@ namespace OsuOszExtractor
             }
             else
             {
-                throw new NotOsuFileException("File '"+path+"' is not osu file");
+                throw new NotOsuFileException("File '" + path + "' is not osu file");
             }
-            
+
         }
 
         public static bool IsOsuFile(string path)
         {
-            FileStream fileStream = new FileStream(path,FileMode.Open);
-            using (ZipArchive archive = new ZipArchive(fileStream))
+            using (FileStream fileStream = new FileStream(path, FileMode.Open))
             {
-                foreach (ZipArchiveEntry entry in archive.Entries)
+                using (ZipArchive archive = new ZipArchive(fileStream))
                 {
-                    if (entry.Name.EndsWith(OSU_FILE_EXTENSION))
+                    foreach (ZipArchiveEntry entry in archive.Entries)
                     {
-                        return true;
+                        if (entry.Name.EndsWith(OSU_FILE_EXTENSION))
+                        {
+                            return true;
+                        }
                     }
-                    
                 }
             }
             return false;
--- OsuOszExtractor/Program.cs
+++ OsuOszExtractor/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OsuOszExtractor
@@ -18,7 +15,7 @@ namespace OsuOszExtractor
             string songsDir = currentDir + "\\" + SONGS_DIR;
             if (!Directory.Exists(songsDir))
             {
-                Console.WriteLine("Songs folder could not be found.");
+                Console.WriteLine("Songs folder could not be found. Expected location: {0}", songsDir);
                 Environment.Exit(1);
             }
             Console.WriteLine("Songs folder found");
@@ -33,36 +30,33 @@ namespace OsuOszExtractor
                 Console.WriteLine(e.Message);
                 Environment.Exit(1);
             }
-            //Extract all zip files
-            foreach (var path in files)
+
+            // Extract all zip files in parallel
+            Parallel.ForEach(files, (path) =>
             {
                 var extension = Path.GetExtension(path);
                 if (extension != ".osz")
                 {
-                    continue;
+                    return;
                 }
-                Path.GetExtension(path);
                 Console.ResetColor();
                 string fileName = Path.GetFileNameWithoutExtension(path);
                 string outputPath = songsDir + "\\" + fileName;
                 try
                 {
-                    Console.Write("Extracting " +fileName + "...");
-                    OsuOszExtrator.ExtractOsz(path,outputPath);
+                    Console.Write("Extracting " + fileName + "...");
+                    OsuOszExtrator.ExtractOsz(path, outputPath);
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.Write("done\n");
-                }                
+                }
                 catch (Exception e)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write("failed\n");
                     Console.ResetColor();
                     Console.WriteLine(e.Message);
-
                 }
-                
-            }
+            });
         }
-        
     }
 }
